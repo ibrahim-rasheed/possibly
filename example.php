5b7871d6e068a84644e860fc38eb4913d8140f3b
ibbe <ibrahim00018@gmail.com>
2022-12-21 19:39:45 +0500

    included English words dictionary and cleanup

--- example.php
+++ example.php
@@ -1,20 +1,27 @@
 <?php
+// include Possibly class
 require_once("Possibly.php");
 
-// elements
-$elements = array('i', 'b', 'b', 'e');
+// initialize Possibly class
+$Combinations = new Possibly();
+
+// open file and process text
+$results = $Combinations->fileToArray("dictionaries/english_words_370k.txt")
+    //->hasAnyRepeated(['b'], 2)
+    //->hasConsecutive(3)
+    //->hasAny(['i', 'b', 'b', 'e'])
+    ->hasAll(['i', 'b', 'e', 'r'])
+    ->toArray();
+
+// print results
+echo "<p>" . count($results) . "</p>";
+echo "<p>" . implode("<br>", $results) . "</p>";
+
+
+//OTHER POSSIBLY STUFF
 
 //a-z array
-$az = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-
-// elements count
-$elementsCount = count($elements);
-
-// length of combinations
-$length = 3;
-
-// initialize Combinations class
-$Combinations = new Possibly($elements);
+//$az = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
 // get combinations count
 //echo "<p>" . $Combinations->getCombinationsCount($elementsCount, $length, false) . "</p>";
@@ -24,13 +31,3 @@ $Combinations = new Possibly($elements);
 
 // open file dictionaries/3-az.txt and filter lines that has the given array of strings
 //echo "<p>" . implode('<br>', $Combinations->hasAnyRepeated("dictionaries/4-az.txt", array('i', 'b', 'r'), 2)) . "</p>";
-
-$results = $Combinations->fileToArray("dictionaries/4-az-19.txt")
-    ->hasAnyRepeated($az, 3)
-    ->hasConsecutive(3)
-    //->hasAny(['i', 'b', 'e', 'r', 's'])
-    //->hasAll(['ib', 'br', 'be', 'ir', 'ra'])
-    ->toArray();
-
-echo "<p>" . count($results) . "</p>";
-echo "<p>" . implode("<br>", $results) . "</p>";
